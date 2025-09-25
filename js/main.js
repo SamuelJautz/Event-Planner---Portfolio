0b51a6a131df335a3ac4fe24669af407fe716903
samuel <samueljautz@gmail.com>
2025-09-25 16:39:17 +0200

    Alle Blöcke aus der Seite speichern und variablen + Prüfung für die Eingabe

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var events = [];
+
+var form = document.getElementById("input-form");
+var title = document.getElementById("title");
+var date = document.getElementById("date");
+var description = document.getElementById("description");
+var eventlist = document.getElementById("event-list");
+var searchbar = document.getElementById("search");
+var sortmenu = document.getElementById("sort");
+
+form.addEventListener("submit", function(e) {
+
+    var t;
+    var d;
+    var desc;
+
+    if(t.length == 0 || d.length == 0 || desc.length == 0) {
+        // TODO: Error einfügfen
+    }
+
+});
+
+
+
+
+
+
+
