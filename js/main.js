e9fe1264a30b64c955a27e851c99406e3f42c297
Samuel <samueljautz@gmail.com>
2025-09-26 10:09:25 +0200

    Überarbeitung der Event-Verwaltung: Einführung der EventItem-Klasse, Verbesserung der Validierung und Aktualisierung der Eventliste. Sortierfunktion angepasst und Fehlerbehandlung für leere Listen hinzugefügt.

--- js/main.js
+++ js/main.js
@@ -9,20 +9,40 @@ var searchbar = document.getElementById("search");
 var sortmenu = document.getElementById("sort");
 
 form.addEventListener("submit", function(e) {
-
-    var event = new Event(titleInput, dateInput, descriptionInput);
-    if(!event.validateEvent){ //TODO: Error Nachricht wenn event nicht validiert werden kann}
-
+  e.preventDefault();
+    var event = new EventItem(titleInput.value, dateInput.value, descriptionInput.value);
+    if(!validateEvent(event)){
+      //TODO: Error Nachricht wenn event nicht validiert werden kann}
+      
+    }
+    events.push(event);
+    sortEvents(events, );
+    refreshEventList(events, eventlist);
 });
 
-function refreshList() {
 
+function refreshEventList(events, eventlist) {
+  eventlist.innerHtml = ""; //alte Listeninhalte entfernen
+
+  //Default -- kiene Events vorhanden
+  if(events.length == 0){
+    eventlist.innerHtml = "<li>Keine Events vorhanden</li>"
+    return;
+  }
+
+  for (var e in events) {
+    var li = document.createElement("li");
+    li.innerHTML = `
+      <h3>${e.title}</h3>
+      <p>Datum: ${e.Date}</p>
+      <p>${e.description}</p>
+    `;
+    eventlist.appendChild(li);
+  }
 }
 
-
-
 // Sortier-Funktion
-function sortEvents(eventlist, sort){
+function sortEvents(events, sort){
   if (mode === "date-asc")   return list.sort((a,b)=> new Date(a.date) - new Date(b.date));
   if (mode === "date-desc")  return list.sort((a,b)=> new Date(b.date) - new Date(a.date));
   if (mode === "title-asc")  return list.sort((a,b)=> a.title.localeCompare(b.title));
@@ -30,9 +50,9 @@ function sortEvents(eventlist, sort){
   return list;
 }
 
-class Event {
+class EventItem {
     constructor(title, date, description) {
-        this.date = title;
+        this.date = title.trim();
         this.date = date;
         this.description = description;
     }
@@ -43,11 +63,3 @@ class Event {
         }
     }
 }
-
-
-
-
-
-
-
-
